b86d54c68a04e53b2ca1acfdc3f132b1e8f7ce0b
dnlruizmoreno <dnlruizmoreno@gmail.com>
2017-10-21 23:31:22 +0200

    [druiz] Modified pom and fixed applicaition

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,16 +8,14 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
 
-        // Create a new SimpleHttpServer
-        UserRolesServer simpleHttpServer;
+        int port = 8030;
         if (args.length == 1 && NumberUtils.isDigits(args[0])){
-            simpleHttpServer = new UserRolesServer(Integer.parseInt(args[0]));
+            port = Integer.parseInt(args[0]);
         }
-        else{
-            simpleHttpServer = new UserRolesServer(8030);
-        }
+
         // Start the server
+        UserRolesServer simpleHttpServer = new UserRolesServer(port);
         simpleHttpServer.start();
-        System.out.println("Server is started and listening on port "+ 8030);
+        System.out.println("Server is started and listening on port "+ port);
     }
 }
