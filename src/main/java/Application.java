1d06b0affc584f5585e55bf85d6c818a5f590efc
dnlruizmoreno <dnlruizmoreno@gmail.com>
2017-10-21 22:56:41 +0200

    [druiz] Added arg in main to custom port of the server

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,5 @@
 import controller.UserRolesServer;
+import org.apache.commons.lang3.math.NumberUtils;
 
 /**
  * Created by danielruizm on 10/20/17.
@@ -8,8 +9,13 @@ public class Application {
     public static void main(String[] args) throws Exception {
 
         // Create a new SimpleHttpServer
-        UserRolesServer simpleHttpServer = new UserRolesServer(8030);
-
+        UserRolesServer simpleHttpServer;
+        if (args[0] != null && NumberUtils.isDigits(args[0])){
+            simpleHttpServer = new UserRolesServer(Integer.parseInt(args[0]));
+        }
+        else{
+            simpleHttpServer = new UserRolesServer(8030);
+        }
         // Start the server
         simpleHttpServer.start();
         System.out.println("Server is started and listening on port "+ 8030);
